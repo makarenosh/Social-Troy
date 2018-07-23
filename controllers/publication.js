2db2418e9f286e594c09d85433d28dcf6485f3eb
Jose <makarenosh@gmail.com>
2018-07-23 14:43:17 +0000

    SUbida de imagenes con clodinaryu

--- controllers/publication.js
+++ controllers/publication.js
@@ -137,7 +137,7 @@ function uploadImage(req, res) {
         //     console.log("El resultado de la subida de la imagen es este de abajo ----> ");
         //     console.log(result);
         // });
-        cloudinary.v2.uploader.upload("file_name",
+        cloudinary.v2.uploader.upload(file_name,
             function(error, result) {
                 if(error){
                     console.log("ERRORRR!!");
