694554384b52eb0eada61c7385c8b9dd18c28cb2
Jose <makarenosh@gmail.com>
2018-08-13 09:10:52 +0000

    Añadiendo comentarios

--- models/comment.js
+++ models/comment.js
@@ -6,7 +6,8 @@ var Schema = mongoose.Schema;
 var CommentSchema = Schema({
     text : String,
     created_at : Date,
-    user : {type: Schema.ObjectId, ref : 'User'}
+    user : {type: Schema.ObjectId, ref : 'User'},
+    publication : {type: Schema.ObjectId, ref : 'Publication'},
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
